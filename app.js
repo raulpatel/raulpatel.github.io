e2281d052e88841c993dde5523ac180b92025034
Raul Patel <raulp@uoregon.edu>
2022-09-26 16:21:33 -0700

    toggle works. testing alternating text

--- app.js
+++ app.js
@@ -12,12 +12,12 @@
         let dark = document.getElementsByClassName("dark-mode");
         let light = document.getElementsByClassName("light");
         for (let i = 0; i < dark.length; i++) {
-            if (dark[i].style.display == "block") dark[i].style.display = "none";
-            else dark[i].style.display = "block";
+            if (dark[i].style.display == "inline-block") dark[i].style.display = "none";
+            else dark[i].style.display = "inline-block";
         };
         for (let i = 0; i < light.length; i++) {
-            if (light[i].style.display == "block") light[i].style.display = "none";
-            else light[i].style.display = "block";
+            if (light[i].style.display == "inline-block") light[i].style.display = "none";
+            else light[i].style.display = "inline-block";
         };
     })
 })();
